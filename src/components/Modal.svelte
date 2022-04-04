c79ca1a3e7fb717b3c8e00b791b3b4b808f6fcf7
Pankaj Kumar <97950640+uc-pkj@users.noreply.github.com>
2022-04-04 23:27:20 +0530

    Update Model.svelte

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,26 +1,29 @@
-	<!-- // File name : Model.svelte
-	// Description : Contain popup when you click end test button
-	// Author : Pankaj Kumar
-	// Version : 1
-	// Package : svelte_items
-	// Created : 15 March 2022
-	// Updated by : Pankaj Kumar
-	// Updated Date : 30 March 2022 -->
-
 <script>
+	/**
+	 * File name : Model.svelte
+	 * Description : Contain popup when you click end test button
+	 * Author : Pankaj Kumar
+	 * Version : 1
+	 * Package : svelte_items
+	 * Created : 15 March 2022
+	 * Updated by : Pankaj Kumar
+	 * Updated Date : 30 March 2022
+	 */
 	import { fade } from 'svelte/transition';
-
-	function overlay_Click(e) {
+	function overlayClick(e) {
 		if ('close' in e.target.dataset) show = false;
 	}
-
 	export let show = false;
 	import ResultPage from '../routes/ResultPage.svelte';
 </script>
 
 {#if show}
 	<div>
-		<div class="modal_Overlay d-flex position-fixed top-0 bottom-0 start-0 end-0" on:click={overlay_Click} transition:fade={{ duration: 150 }}>
+		<div
+			class="modal_Overlay d-flex position-fixed top-0 bottom-0 start-0 end-0"
+			on:click={overlayClick}
+			transition:fade={{ duration: 150 }}
+		>
 			<div class=" shadow position-relative bg-light rounded mx-auto my-auto ">
 				<main class="p-3 d-flex justify-content-center align-items-center flex-column">
 					<h2 class=" text-dark text-capitalize m-0">Are you want to end your test ?</h2>
@@ -30,7 +33,9 @@
 							<button class="rounded p-2 m-3 w-100" accesskey="y"> YES </button>
 						</a>
 						<!-- svelte-ignore a11y-accesskey -->
-						<button class="rounded px-4 py-2 m-4 " on:click={() => (show = false)} accesskey="n"> NO </button>
+						<button class="rounded px-4 py-2 m-4 " on:click={() => (show = false)} accesskey="n">
+							NO
+						</button>
 					</div>
 				</main>
 			</div>
@@ -42,5 +47,4 @@
 	.modal_Overlay {
 		background: rgba(0, 0, 0, 0.5);
 	}
-	
 </style>
