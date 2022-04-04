59d5bf628ebccb86450ca124279cfe71df857408
Pankaj Kumar <97950640+uc-pkj@users.noreply.github.com>
2022-04-04 23:26:50 +0530

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,15 @@
-<!-- 
-	// File name : Header.svelte
-	// Description : Contain header (top bar) of all components.
-	// Author : Pankaj Kumar
-	// Version : 1
-	// Package : svelte_items
-	// Created : 15 March 2022
-	// Updated by : Pankaj Kumar
-	// Updated Date : 30 March 2022  -->
+<script>
+	/**
+	 * File name : Header.svelte
+	 * Description : Contain header (top bar) of all components.
+	 * Author : Pankaj Kumar
+	 * Version : 1
+	 * Package : svelte_items
+	 * Created : 15 March 2022
+	 * Updated by : Pankaj Kumar
+	 * Updated Date : 30 March 2022
+	 */
+</script>
 
 <main>
 	<header class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
